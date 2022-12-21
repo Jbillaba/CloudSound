6f8a424eaad446b35bf6c72d5010b995741f472f
Jeremy <jeremyvillalva66@gmail.com>
2022-12-21 18:19:23 -0500

    resolved leak

--- cloudsound_project/cloudsound_django/settings.py
+++ cloudsound_project/cloudsound_django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$lvgze+uv(zev^&$rn$8_)xha^*ox*^z9gi+h=x92ug6l9oe0)'
+SECRET_KEY = {DJANGO_KEY}
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
