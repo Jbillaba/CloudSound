dcc0725109c2d961d1aebd9ccb7df35c7fd91b46
Jeremy <jeremyvillalva66@gmail.com>
2022-12-21 18:10:08 -0500

    began setting up the db

---
+++ cloudsound_project/cloudsound/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CloudsoundConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cloudsound'
---
+++ cloudsound_project/settings.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE cloudsound ; 
+CREATE USER cloudsounduser WITH PASSWORD 'cloudsound' ; 
+GRANT ALL PRIVILEGES ON DATABASE cloudsound TO cloudsounduser ; 
