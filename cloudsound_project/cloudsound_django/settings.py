487f55d56b7d8ac2cf6b2d602cb8d4406fbc1c39
Jeremy <jeremyvillalva66@gmail.com>
2022-12-22 12:06:20 -0500

    added front end

--- cloudsound_project/cloudsound_django/settings.py
+++ cloudsound_project/cloudsound_django/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = {DJANGO_KEY}
+SECRET_KEY = 'django-insecure-$lvgze+uv(zev^&$rn$8_)xha^*ox*^z9gi+h=x92ug6l9oe0)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
