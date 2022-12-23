f4ebf1fc276c600cb660d72f878064355ca3331d
Jeremy <jeremyvillalva66@gmail.com>
2022-12-23 10:55:01 -0500

    need to work on the backend

--- cloudsound/src/Components/TEST.jsx
+++ cloudsound/src/Components/TEST.jsx
@@ -5,23 +5,28 @@ import {  BASE_URL_SONGS } from '../globals'
 
 export default function Test() {
 
-    const [songs, setSongs] = useState([])
+    const [songs, setSongs] = useState(null)
+
+
 
     useEffect(() => {
         const getSongs = async () => {
-            const response = await axios.get("http://localhost:8000/songs/")
+            const response = await axios.get(`${BASE_URL_SONGS}`)
             setSongs(response.data)
-            console.log(response.data)
+            // console.log(response.data)
         }
         
         getSongs()
     }, [])
 
+    console.log(songs)
+
     return (
         <div>
-            <h2>
-                this is a test to see if the audio works from the database 
-            </h2>
+            <h2> this is a test to see if the audio works from the database </h2>
+            <h2>{songs.name}</h2>
+            <audio src={songs.audio_file}></audio>
         </div>
+
     )
     }
