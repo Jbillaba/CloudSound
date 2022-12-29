d421de0553c18b96495047b483491caf4aaa9452
Jeremy <jeremyvillalva66@gmail.com>
2022-12-29 15:54:46 -0500

    started working on a navbar

--- cloudsound/src/App.js
+++ cloudsound/src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import Discover from './Components/Discover';
 import Landing from './Components/Landing';
 import Main from './Components/Main';
+import NavBar from './Components/navbar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Main/>
         
     </div>
---
+++ cloudsound/src/Components/navbar.jsx
@@ -0,0 +1,7 @@
+export default function NavBar () {
+    return (
+        <div>
+            <h3> this is the nav bar </h3>
+        </div>
+    )
+}
