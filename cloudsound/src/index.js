338c4ae89df4453f1a8fa88f0dbe17e810c5bd6f
Jeremy <jeremyvillalva66@gmail.com>
2022-12-30 13:40:34 -0500

    added auth0 provider to index.js

--- cloudsound/src/index.js
+++ cloudsound/src/index.js
@@ -4,11 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <Auth0Provider>
     <App />
+    </Auth0Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
