442f4a45a27de51acef70a9d1b55657ea6cebcae
Jeremy <jeremyvillalva66@gmail.com>
2022-12-30 11:43:26 -0500

    added captains log to users

--- cloudsound/src/Components/User.jsx
+++ cloudsound/src/Components/User.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react"
 import { BASE_URL_USERS } from "../globals"
 import axios from 'axios'
-
+// 12/20/22 11:42 am captains log: should be working on auth to ensure it works and shit although i dont deem it necessary i should still do it
 export default function User () {
 
-    const [ user, setUser ] = useState(null)
+    const [ user, setUser ] = useState([])
 
     useEffect (() => {
         const getUsers = async () => {
@@ -15,12 +15,10 @@ export default function User () {
         getUsers()
     }, [])
 
-
+    if(!user) return <h2> Loading...</h2>
     return (
         <div className="user-container">
-            <h1>Welcome back</h1>
-            <h2> youre name is {user.name}</h2>
-            {/* <h3> id num. is {user.id} </h3> */}
+            <h1>Welcome back, {user.name}</h1>
         </div>
     )
 }
