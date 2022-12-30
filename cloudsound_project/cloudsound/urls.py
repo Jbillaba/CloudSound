46af22b63afb2046213b5587b366d0aab8d0c028
Jeremy <jeremyvillalva66@gmail.com>
2022-12-30 17:18:09 -0500

    working on post request thru front end

--- cloudsound_project/cloudsound/urls.py
+++ cloudsound_project/cloudsound/urls.py
@@ -51,6 +51,7 @@ urlpatterns = [
   path('', include(router.urls)),
   path('admin/', admin.site.urls),
   path('api/', include('rest_framework.urls')),
+  path('', views.index, name="index"),
   path("login", views.login, name="login"),
   path("logout", views.logout, name="logout"),
   path("callback", views.callback, name="callback")
