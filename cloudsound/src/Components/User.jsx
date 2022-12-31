ce367bb142904aae0a4e0a1a5aadeb18f2b1d235
Jeremy <jeremyvillalva66@gmail.com>
2022-12-31 11:18:15 -0500

    set up the form

--- cloudsound/src/Components/User.jsx
+++ cloudsound/src/Components/User.jsx
@@ -1,12 +1,16 @@
 import { useState, useEffect } from "react"
 import { BASE_URL_USERS } from "../globals"
 import axios from 'axios'
+import {useContext} from 'react'
+import AuthContext from "../context/AuthContext"
+
 
 
 export default function User () {
+    let {loginUser} = useContext(AuthContext)
     return (
         <div>
-         <form>
+         <form onSubmit={loginUser}>
             <input type="email" name ="email" placeholder='Enter Your email' />
             <input type="password" name ="password" placeholder='Enter Your password' />
             <input type='submit'/>
--- cloudsound/src/context/AuthContext.js
+++ cloudsound/src/context/AuthContext.js
@@ -10,8 +10,20 @@ export const AuthProvider = ({children}) => {
     let [authTokens, setAuthTokens] = useState(null)
     let [user, setUser] = useState(null)
 
+    let loginUser = async (e ) => {
+        e.preventDefault()
+        console.log('form submitted')
+        // let response = fetch('http://localhost:8000/api/token/',{ 
+        // method:'POST', 
+        // headers: {
+        //     'content-type':'application/json'
+        // }, 
+        // body:JSON.stringify({'name':null, 'password':null})
+        // }) 
+    }
+
     let contextData = {
-       
+       loginUser:loginUser
     }
 
     return (
