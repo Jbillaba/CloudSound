0d9f14fc2b1438c40d44fdad42ce7b28df7198b1
Jeremy <jeremyvillalva66@gmail.com>
2022-12-30 20:21:31 -0500

    finsished jwt setup

--- cloudsound_project/cloudsound/urls.py
+++ cloudsound_project/cloudsound/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from cloudsound.views import SongViewSet, UserViewSet, PlaylistViewSet
 from . import views
 from django.contrib import admin 
-
+from .views import MyTokenObtainPairView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView
@@ -57,7 +57,7 @@ urlpatterns = [
   path('', include(router.urls)),
   path('admin/', admin.site.urls),
   path('api/', include('rest_framework.urls')),
-  path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+  path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
   path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 
 ] 
--- cloudsound_project/cloudsound/views.py
+++ cloudsound_project/cloudsound/views.py
@@ -2,6 +2,9 @@ from rest_framework import viewsets, parsers
 from .serializers import SongSerializer, UserSerializer, PlaylistSerializer
 from .models import Song,User,playlist
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 
 # create views here 
 class SongViewSet(viewsets.ModelViewSet):
@@ -19,3 +22,19 @@ class PlaylistViewSet(viewsets.ModelViewSet):
     serializer_class = PlaylistSerializer
     parser_classes = [parsers.MultiPartParser, parsers.FormParser]
     http_method_names = ['get', 'post', 'patch', 'delete']
+
+# jwt 
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['email'] = user.email
+        # ...
+
+        return token 
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
