387e310ccb5fab3509eb53aee6e2356b63c08ddf
Jeremy <jeremyvillalva66@gmail.com>
2023-01-03 10:06:01 -0500

    added logout function

--- cloudsound/src/Components/navbar.jsx
+++ cloudsound/src/Components/navbar.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom"
 import AuthContext from "../context/AuthContext"
 
 export default function NavBar () {
-    let {user} = useContext(AuthContext)
+    let {user, logoutUser} = useContext(AuthContext)
     return (
        <ul className="nav-bar">
         <Link to='/discover'>Discover</Link>
         <Link to='/upload'>Upload</Link>
         
         {user ? (
-            <p>Logout</p>
+            <p onClick={logoutUser}>Logout</p>
         ): (
             <Link to='/login'>Login</Link>
         )}
