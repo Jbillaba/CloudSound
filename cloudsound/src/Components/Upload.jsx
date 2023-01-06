bebd7cb6283b1b832510e35640a77761d415d528
Jeremy <jeremyvillalva66@gmail.com>
2023-01-06 01:20:28 -0500

    made upload form

--- cloudsound/src/Components/Upload.jsx
+++ cloudsound/src/Components/Upload.jsx
@@ -1,14 +1,19 @@
 import { useState } from "react"
-
+import { useContext } from "react"
+import AuthContext from "../context/AuthContext"
 
 export default function Upload () {
-
+let {uploadSong} = useContext(AuthContext)
     return (
         <div>
             <h1>Upload A Song</h1>
- 
-
-            
+                <form onSubmit={uploadSong}>
+                        <input type="text" name="name" placeholder="Enter Song Name" />
+                        <p> enter Cover Art... </p>
+                        <input type='file' name="image" accept="image/*"  />
+                        <p> enter audio file... </p>
+                        <input type='file' name='audio_file' accept='audio/*'/>
+                </form>
         </div>
     )
 }
